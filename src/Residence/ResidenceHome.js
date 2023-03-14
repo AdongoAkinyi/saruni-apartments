6370d8d20a9effdefb58646bc1472a7548d9b6a0
Olivia Adongo <akinyioliviatove@gmail.com>
2023-03-14 13:03:15 +0300

    Milestone: Recidence homepage page completed

--- src/Residence/ResidenceHome.js
+++ src/Residence/ResidenceHome.js
@@ -1,15 +1,23 @@
 import "./Residence.css";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 function ResidenceHome() {
   let navigate = useNavigate();
   return (
-    <div class="grid-container-element">
-      <div class="grid-child-element purple">
+    <div class="grid-container-element"> 
+    
+      <div className="grid-child-element purple">
         <div className="content">
-        <img  src="http://www.thesaruni.com/TheSaruniImages/SaruniLogo.png" className="residence-home-logo"/>
-          <h4 className="reside">RESIDE IN LUXURY</h4>
-          <h5 className="reside paragraph"> surrounded by spectacular uninterrupted views</h5>
+        <img  src="http://www.thesaruni.com/TheSaruniImages/SaruniLogo.png" className="residence-home-logo" alt="logo"/>
+          <h4 className="reside">R E S I D E  </h4>
+          <h4 className="reside" style={{marginTop:"15px", marginBottom:"15px"}}> I  N  
+          
+            L U X U R Y</h4>
+          
+          <h5 className="reside-paragraph"> surrounded by spectacular </h5>
+          <h5 className="reside-paragraph" style={{marginTop:"15px", marginBottom:"15px"}}>  uninterrupted views</h5>
+
           <button
             onClick={() => {
               navigate("/residence");
@@ -20,15 +28,18 @@ function ResidenceHome() {
           </button>
         </div>
       </div>
-      <div class="grid-child-element green">
+
+      <div className="grid-child-element green">
         <img
 
           src="https://i.pinimg.com/564x/e2/4d/33/e24d33bf0e8d0c0970a4452d7ce8f8d9.jpg"
           alt="residence"
           className="residenceimagehome"
+          
         />
       </div>
     </div>
+    
   );
 }
 
